504f98a8913108415f0969d8ce61a481c162ecc2
Kenny Williams <kennyjwilli@gmail.com>
2016-02-22 19:10:03 -0800

    Add varargs to onSuccess

--- app/src/main/java/com/postalmessanger/messenger/util/Fn.java
+++ app/src/main/java/com/postalmessanger/messenger/util/Fn.java
@@ -3,7 +3,12 @@ package com.postalmessanger.messenger.util;
 /**
  * Created by kenny on 2/18/16.
  */
-public interface Fn {
-    void onSuccess();
-    void onError();
+public abstract class Fn {
+    public void onSuccess(Object... args) {}
+
+    public void onSuccess() {
+        onSuccess(new Object[]{});
+    }
+
+    public abstract void onError();
 }
