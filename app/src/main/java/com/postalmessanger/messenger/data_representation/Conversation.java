afcc4ca35c2e20076e3982555d50f5a741475141
Kenny Williams <kennyjwilli@gmail.com>
2016-02-25 00:21:02 -0800

    Add class to store conversation

---
+++ app/src/main/java/com/postalmessanger/messenger/data_representation/Conversation.java
@@ -0,0 +1,22 @@
+package com.postalmessanger.messenger.data_representation;
+
+import com.postalmessanger.messenger.util.Util;
+
+/**
+ * Created by kenny on 2/24/16.
+ */
+public class Conversation {
+    public String thread_id;
+    public long timestamp;
+    public String date;
+    public String address;
+    public String text;
+
+    public Conversation(String thread_id, long timestamp, String address, String text) {
+        this.thread_id = thread_id;
+        this.timestamp = timestamp;
+        this.date = Util.formatTimestamp(timestamp);
+        this.address = Util.normalizePhoneNumber(address);
+        this.text = text;
+    }
+}
