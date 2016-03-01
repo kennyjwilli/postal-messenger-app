8a29e9f32f474aa86b60feefe3597cd334f7a76e
Kenny Williams <kennyjwilli@gmail.com>
2016-02-29 17:16:53 -0800

    Move phone number type to PhoneNumber

--- app/src/main/java/com/postalmessanger/messenger/data_representation/PhoneNumber.java
+++ app/src/main/java/com/postalmessanger/messenger/data_representation/PhoneNumber.java
@@ -1,6 +1,6 @@
 package com.postalmessanger.messenger.data_representation;
 
-import com.postalmessanger.messenger.util.Util;
+import android.provider.ContactsContract;
 
 /**
  * Created by kenny on 2/19/16.
@@ -15,7 +15,7 @@ public class PhoneNumber {
     }
 
     public PhoneNumber(int type, String phoneNumber) {
-        this(Util.getPhoneNumberTypeString(type), phoneNumber);
+        this(getType(type), phoneNumber);
     }
 
     public PhoneNumber(String phoneNumber) {
@@ -23,6 +23,76 @@ public class PhoneNumber {
     }
 
     public PhoneNumber() {
+    }
+
+    public static String getType(int type) {
+        String sType = "";
+        switch (type) {
+            case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                sType = "Home";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                sType = "Mobile";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                sType = "Work";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_FAX_HOME:
+                sType = "Home Fax";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_FAX_WORK:
+                sType = "Work Fax";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_MAIN:
+                sType = "Main";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_OTHER:
+                sType = "Other";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_CUSTOM:
+                sType = "Custom";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_PAGER:
+                sType = "Pager";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_ASSISTANT:
+                sType = "Assistant";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_CALLBACK:
+                sType = "Callback";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_CAR:
+                sType = "Car";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_COMPANY_MAIN:
+                sType = "Company Main";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_ISDN:
+                sType = "ISDN";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_MMS:
+                sType = "MMS";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_OTHER_FAX:
+                sType = "Other Fax";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_RADIO:
+                sType = "Radio";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_TELEX:
+                sType = "Telex";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_TTY_TDD:
+                sType = "TTY TDD";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_WORK_MOBILE:
+                sType = "Work Mobile";
+                break;
+            case ContactsContract.CommonDataKinds.Phone.TYPE_WORK_PAGER:
+                sType = "Work Pager";
+                break;
+        }
+        return sType;
     }
 
     public String toString() {
--- app/src/main/java/com/postalmessanger/messenger/util/Util.java
+++ app/src/main/java/com/postalmessanger/messenger/util/Util.java
@@ -127,76 +127,6 @@ public class Util {
         smsManager.sendTextMessage(number, null, text, piSent, null);
     }
 
-    public static String getPhoneNumberTypeString(int type) {
-        String sType = "";
-        switch (type) {
-            case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
-                sType = "Home";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
-                sType = "Mobile";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
-                sType = "Work";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_FAX_HOME:
-                sType = "Home Fax";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_FAX_WORK:
-                sType = "Work Fax";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_MAIN:
-                sType = "Main";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_OTHER:
-                sType = "Other";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_CUSTOM:
-                sType = "Custom";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_PAGER:
-                sType = "Pager";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_ASSISTANT:
-                sType = "Assistant";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_CALLBACK:
-                sType = "Callback";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_CAR:
-                sType = "Car";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_COMPANY_MAIN:
-                sType = "Company Main";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_ISDN:
-                sType = "ISDN";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_MMS:
-                sType = "MMS";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_OTHER_FAX:
-                sType = "Other Fax";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_RADIO:
-                sType = "Radio";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_TELEX:
-                sType = "Telex";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_TTY_TDD:
-                sType = "TTY TDD";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_WORK_MOBILE:
-                sType = "Work Mobile";
-                break;
-            case ContactsContract.CommonDataKinds.Phone.TYPE_WORK_PAGER:
-                sType = "Work Pager";
-                break;
-        }
-        return sType;
-    }
-
     public static List<PhoneNumber> phoneNumbersFor(Context ctx, String id) {
         List<PhoneNumber> phoneNumbers = new ArrayList<>();
         ContentResolver cr = ctx.getContentResolver();
@@ -209,7 +139,7 @@ public class Util {
             while (pCur.moveToNext()) {
                 int type = pCur.getInt(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
                 String phoneNumber = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                phoneNumbers.add(new PhoneNumber(getPhoneNumberTypeString(type), phoneNumber));
+                phoneNumbers.add(new PhoneNumber(type, phoneNumber));
             }
             pCur.close();
         }
